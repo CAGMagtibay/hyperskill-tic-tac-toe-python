063954916c2c9011222f29b425b97801a55a1b89
CAGMagtibay <41831788+CAGMagtibay@users.noreply.github.com>
2020-07-29 17:39:51 -0400

    Create tictactoe.py

---
+++ stage-2/tictactoe.py
@@ -0,0 +1,9 @@
+# write your code here
+field_input = input("Enter cells: ")
+char_list = list(field_input)
+
+print("---------")
+print("| %s %s %s |" %(char_list[0], char_list[1], char_list[2]))
+print("| %s %s %s |" %(char_list[3], char_list[4], char_list[5]))
+print("| %s %s %s |" %(char_list[6], char_list[7], char_list[8]))
+print("---------")
